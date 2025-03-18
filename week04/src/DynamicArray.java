2647c90e588635fde81592c54c2e8fd1265c065a
fQwQf <supertjz123@foxmail.com>
2025-03-18 12:03:05 +0800

    feat(week04): 实现 DynamicArray 动态数组功能
    - 新增 DynamicArray 类，包含添加、删除、获取元素等功能
    - 实现动态扩容和缩容，以适应元素数量的变化
    - 更新 Main 类，演示 DynamicArray 的使用方法

---
+++ week04/src/DynamicArray.java
@@ -0,0 +1,52 @@
+public class DynamicArray {
+    private int[] array;
+    private int size;
+
+    public DynamicArray() {
+        array = new int[2];
+        size = 0;
+    }
+
+    public void add(int element) {
+        if (size == array.length) {
+            resize(array.length * 2);
+        }
+        array[size++] = element;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        if (size > 0 && size <= array.length / 4) {
+            resize(array.length / 2);
+        }
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return array.length;
+    }
+
+    private void resize(int newCapacity) {
+        int[] newArray = new int[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+}
