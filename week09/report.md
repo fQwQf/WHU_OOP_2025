7e71a202901f5b919dd1172ef4ed59015cb6e30d
fQwQf <supertjz123@foxmail.com>
2025-04-20 17:07:54 +0800

    refactor(week09): 优化 TestMyBox 类的输出方法并完善测试
    - 将 System.out.println 中的字符串拼接改为使用 toString() 方法
    - 增加了 String Box 与 doubleBox 的 equals 比较，完善了测试用例

---
+++ week09/report.md
@@ -0,0 +1,12 @@
+TestMyBox运行结果：
+```shell
+/home/fqwqf/.jdks/openjdk-24/bin/java -javaagent:/home/fqwqf/桌面/idea-IC-243.25659.59/lib/idea_rt.jar=33417 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/fqwqf/桌面/project/WHU_OOP_2025/week09/out/production/week09 TestMyBox
+MyBox{item=Hello, World!}
+MyBox{item=123}
+MyBox{item=3.14}
+String Box equals anotherStringBox: true
+String Box equals integerBox: false
+String Box equals doubleBox: false
+
+进程已结束，退出代码为 0
+```
--- week09/src/TestMyBox.java
+++ week09/src/TestMyBox.java
@@ -2,20 +2,21 @@ public class TestMyBox {
     public static void main(String[] args) {
         MyBox<String> stringBox = new MyBox<>();
         stringBox.set("Hello, World!");
-        System.out.println("String Box: " + stringBox);
+        System.out.println(stringBox.toString());
 
         MyBox<Integer> integerBox = new MyBox<>();
         integerBox.set(123);
-        System.out.println("Integer Box: " + integerBox);
+        System.out.println(integerBox.toString());
 
         MyBox<Double> doubleBox = new MyBox<>();
         doubleBox.set(3.14);
-        System.out.println("Double Box: " + doubleBox);
+        System.out.println(doubleBox.toString());
 
         MyBox<String> anotherStringBox = new MyBox<>();
         anotherStringBox.set("Hello, World!");
 
         System.out.println("String Box equals anotherStringBox: " + stringBox.equals(anotherStringBox));
         System.out.println("String Box equals integerBox: " + stringBox.equals(integerBox));
+        System.out.println("String Box equals doubleBox: " + stringBox.equals(doubleBox));
     }
 }
