3d6169fe83e45632b2709f5a1f132aa7b7d87f96
fQwQf <supertjz123@foxmail.com>
2025-04-24 08:19:43 +0800

    refactor(week09): 更新 TestProcessor 类并添加运行结果
    - 修改 TestProcessor 类中的字符串列表，使用更通用的示例数据
    - 移除了一些未使用的代码片段，精简了 main 方法
    - 在报告中添加了 TestProcessor 的运行结果

--- week09/report.md
+++ week09/report.md
@@ -157,3 +157,17 @@ public class TestProcessor {
 
 }
 ```
+
+
+TestProcessor运行结果：
+```shell
+/home/fqwqf/.jdks/openjdk-24/bin/java -javaagent:/home/fqwqf/桌面/idea-IC-243.25659.59/lib/idea_rt.jar=36553 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/fqwqf/桌面/project/WHU_OOP_2025/week09/out/production/week09 TestProcessor
+Processing Strings:
+Printing: Hello
+Printing: World
+Printing: fQwQf
+Sum of Numbers: 15
+Collected list: [1, 2, 3]
+
+进程已结束，退出代码为 0
+```
--- week09/src/TestProcessor.java
+++ week09/src/TestProcessor.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class TestProcessor {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("Java", "Generics", "Example");
+        List<String> strings = Arrays.asList("Hello", "World", "fQwQf");
         StringPrinter stringPrinter = new StringPrinter();
         System.out.println("Processing Strings:");
         ProcessorUtil.apply(strings, stringPrinter);
@@ -15,14 +15,9 @@ public class TestProcessor {
         System.out.println("Sum of Numbers: " + numberSummer.getSum());
 
         // 拓展内容测试
-        List<Number> numberList = Arrays.asList(1, 2.0, 3, 4.5);
-        List<Object> objectList = Arrays.asList("a", 1, 2.0, true);
-        ProcessorUtil.apply(numberList, item -> System.out.println(item));
-
         List<Object> dest = new ArrayList<>();
         List<Integer> src = Arrays.asList(1,2,3);
         collect(dest, src);
-
         System.out.println("Collected list: "+dest);
 
     }
